8fb879aeb7dc1cad3e8c9e059d870fb0ffdea6fe
harbinman <harbinman@163.com>
2026-06-27 21:01:17 +0800

    new update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',       // 🌟 核心：开启纯静态导出模式
+  images: {
+    unoptimized: true,    // 🌟 必须：静态导出模式下，必须禁用 Next.js 的图片优化组件
+  },
+};
 
 module.exports = nextConfig;
